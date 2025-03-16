05afb1374b2252dfefd4ae826d8de67262554218
mxxikr <mxxikr@gmail.com>
2025-03-17 08:06:20 +0900

    1강 실습 코드 작성

---
+++ java_programming/src/knou/java/mxxikr/assignment/thefrist/Hello.java
@@ -0,0 +1,11 @@
+package knou.java.mxxikr.assignment.thefrist;
+
+/**
+ * The Hello class implements an application that
+ * simply prints "Hello, Java Application! " to standard output.
+ */
+public class Hello { // 클래스 정의 블록
+    public static void main(String args[]) { // 프로그램 실행시 main  함수 필요
+        System.out.println("Hello, Java Application!"); // 출력문
+    }
+}
